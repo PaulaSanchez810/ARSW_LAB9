a19b4d3a368699771894f0e0d699eb4a0b83dd8d
Daniel R <daniel.rincon96@gmail.com>
2021-04-22 12:01:21 -0500

    Recursive function#

--- FunctionProject/Fibonacci/index.js
+++ FunctionProject/Fibonacci/index.js
@@ -2,24 +2,24 @@ var bigInt = require("big-integer");
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let nth = req.body.nth
-    let nth_1 = bigInt.one;
-    let nth_2 = bigInt.zero;
+    let nth = context.req.query.nth;
     let answer = bigInt.zero;
+    let memo = {};
 
-    if (nth < 0)
-        throw 'must be greater than 0'
-    else if (nth === 0)
-        answer = nth_2
-    else if (nth === 1)
-        answer = nth_1
-    else {
-        for (var i = 0; i < nth - 1; i++) {
-            answer = nth_2.add(nth_1)
-            nth_2 = nth_1
-            nth_1 = answer
+    function fib(n, a = bigInt.zero, b = bigInt.one) {
+        if (n in memo) return memo[n];
+        if (n == 0) {
+            memo[n] = a;
+            return a;
         }
+        if (n == 1) {
+            memo[n] = b;
+            return b;
+        }
+        return memo[n] = fib(n - 1, b, a.add(b));
     }
+
+    answer = fib(nth);
 
     context.res = {
         body: answer.toString()
